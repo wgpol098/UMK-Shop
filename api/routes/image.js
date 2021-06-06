a958848fc37cf919d88ec7d10eb6036e7c28917b
wgpol098@gmail.com <wgpol098@gmail.com>
2021-06-06 19:54:26 +0200

    Fixed

--- api/routes/image.js
+++ api/routes/image.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const fs = require('fs');
 
 //Metoda odpowiedzialna za dodawanie zdjęcia
 //TODO: Do przetestowania
 //TODO: Zrobić dokumentację
 router.post('/', function(req, res, next)
 {
-    console.log(req.files.image);
-    
-    const path = req.files.image.name;
+    const path = "./img/" + req.files.image.name;
 
-    req.files.image.mv("./img/" + path, function(err)
+    req.files.image.mv(path, function(err)
     {
         if (err) return res.sendStatus(500);
-        res.send(path);
+        res.send({filepaht: path});
     })
 });
 
